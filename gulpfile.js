edcdb6e11d45db58dff88ad5390f39d53c327fc3
jk-5 <jeffreykog@me.com>
2015-05-18 17:14:03 +0200

    Removed 2 unneeded files

---
+++ gulpfile.js
@@ -0,0 +1,107 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var through2 = require('through2');
+var sourcemaps = require('gulp-sourcemaps');
+var ts = require('gulp-typescript');
+var concat = require('gulp-concat');
+var stylus = require('gulp-stylus');
+var rename = require('gulp-rename');
+var minifyCSS = require('gulp-minify-css');
+var del = require('del');
+var runSeq = require('run-sequence');
+
+// Stylus includes
+var nib = require('nib');
+var jeet = require('jeet');
+
+// Error handling
+var handleError = function (error) {
+    gutil.log(error.message);
+    this.emit('end');
+};
+
+// Stylus build configuration
+var styl = function () {
+    return stylus({
+        use: [
+            nib(),
+            jeet()
+        ]
+    });
+};
+
+// Cleans the build directory
+gulp.task('clean', function (cb) {
+    del([
+        './build/'
+    ], cb);
+});
+
+// Copies html files
+gulp.task('copy:html', function () {
+    return gulp.src(['./src/html/**/*'])
+        .pipe(gulp.dest('./build/'))
+});
+
+// Copies static files
+gulp.task('copy:static', function () {
+    return gulp.src(['./src/static/**/*'])
+        .pipe(gulp.dest('./build/static/'))
+});
+
+// Compiles and minifies stylus files
+gulp.task('build:styl', function () {
+    return gulp.src(['./src/styl/index.styl'])
+        .pipe(styl())
+        .pipe(rename("pumpkin.css"))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./build/static/'));
+});
+
+// Compiles and minifies typescript files
+gulp.task('build:ts', function () {
+    return gulp.src(['./src/ts/**/*.ts'])
+        .pipe(ts({out: 'pumpkin.js'}))
+        //.pipe(uglify())
+        .pipe(gulp.dest('./build/static'));
+});
+
+// Performs build
+gulp.task('build', function (cb) {
+    runSeq(
+        'clean',
+        ['copy:html', 'copy:static', 'build:ts', 'build:styl'],
+        cb
+    );
+});
+
+// Compiles stylus with sourcemaps
+gulp.task('build:styl-dev', function () {
+    return gulp.src('./src/styl/index.styl')
+        .pipe(sourcemaps.init())
+        .pipe(styl())
+        .on('error', handleError)
+        .pipe(sourcemaps.write())
+        .pipe(rename('pumpkin.css'))
+        .pipe(gulp.dest('./build/static/'));
+});
+
+// Compiles typescript with sourcemaps
+gulp.task('build:ts-dev', function () {
+    return gulp.src(['./src/ts/**/*.ts'])
+        .pipe(sourcemaps.init())
+        .pipe(ts({out: 'pumpkin.js'}))
+        //.pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./build/static'));
+});
+
+gulp.task('build-dev', ['copy:html', 'copy:static', 'build:styl-dev', 'build:ts-dev']);
+
+// Rebuild when files are changed
+gulp.task('dev', ['build-dev'], function () {
+    gulp.watch('./src/**/*', ['build-dev']);
+});
+
+// Make dev the default task
+gulp.task('default', ['dev']);
